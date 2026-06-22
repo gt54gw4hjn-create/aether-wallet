f8414f310ec9e0f35f7a5a96c8ddc8a3cbca9e30
Aether Wallet User <user@example.com>
2026-06-22 19:50:23 +0800

    chore: remove temp fix script

--- fix_apptsx.ps1
+++
@@ -1,4 +0,0 @@
-$lines = Get-Content 'src\App.tsx'
-$out = $lines[0..2760] + $lines[2892..($lines.Length-1)]
-Set-Content 'src\App.tsx' $out -Encoding UTF8
-Write-Host ('Done. Lines now: ' + (Get-Content 'src\App.tsx').Count)
